5e3383babbbf96d76c40b49f5a7f795d331a9de4
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-22 22:27:07 +0900

    Ch. 10 - ProfileControllerTest 수정

--- src/main/java/com/davidy87/book/springboot/web/ProfileController.java
+++ src/main/java/com/davidy87/book/springboot/web/ProfileController.java
@@ -19,7 +19,7 @@ public class ProfileController {
     @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real1", "real2", "real3");
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
 
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
 
